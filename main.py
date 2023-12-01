a2866499b6a0405d01023d04d642577df007525c
ZachMedal <142929998+ZachMedal@users.noreply.github.com>
2023-12-01 12:03:54 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# This file was created by Zach Medal on 11/16/23
+# Sources:
+# Title: Fall
+# Goals: 
+    # Mobile Game
+    # Laser
+    # Character rolling down
+    # Multiple platforms
+    # Powerups
+    # Time stamp
+    # Score count
